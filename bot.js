5c986e139ae340939a89a507952563818bb3dd6a
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-05-19 23:17:08 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+var Chance = require('chance');
+var chance = new Chance();
+
+const client = new Discord.Client();
+const config = require("./config.json");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+
+  if(command === "meething") {
+    // Return a random meething room
+    var randomRoom = chance.animal() +"_"+ chance.first() +"_"+ chance.city();
+    var conference = "Videoroom Ready at: https://us.meething.space/?room="+randomRoom;
+    message.channel.send(conference);
+  }
+
+});
+
+client.login(config.token);
